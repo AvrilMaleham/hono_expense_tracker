071df114ee8f42e487ec6cfc5dc4dbeb52ecb96d
Avril <avrilmaleham@outlook.com>
2025-10-02 15:34:21 +1300

    chore: trying to rename dashboard

--- apps/ui/src/routes/index.tsx
+++ apps/ui/src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Dashboard } from "@/components/dashboard";
+import { Dashboard } from "@/components/dashboardnew";
 
 export const Route = createFileRoute("/")({
   component: Dashboard,
